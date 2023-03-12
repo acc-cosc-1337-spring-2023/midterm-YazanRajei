f96c3ca3aef45a029ee9cabd6ef4434d34683352
YazanRajei <123427637+YazanRajei@users.noreply.github.com>
2023-03-12 11:12:02 -0500

    Update main.cpp

--- src/question_1/main.cpp
+++ src/question_1/main.cpp
@@ -1,6 +1,29 @@
+#include "question1.h"
 
+using std:: cin; using std::cout; 
 int main()
 {
 
+int n;
+    char choice = 'y';
+
+    while (choice == 'y') 
+    {
+        cout << "Enter a positive integer between 1 and 15: ";
+        cin >> n;
+
+        while (n < 1 || n > 15) {
+            cout << "Invalid input. Please enter a positive integer between 1 and 15: ";
+            cin >> n;
+        }
+
+        string sequence = get_fib_sequence(n);
+        cout << "The Fibonacci sequence up to " << n << " is: " << sequence << "\n";
+
+        cout << "Do you want to continue? (y/n): ";
+        cin >> choice;
+        
+    }
+
     return 0;
-}
+    }
