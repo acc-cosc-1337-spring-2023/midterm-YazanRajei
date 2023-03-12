c58d81f50ff8daf6e2f78b254d33c5e730db939e
YazanRajei <123427637+YazanRajei@users.noreply.github.com>
2023-03-12 11:21:10 -0500

    Update main.cpp

--- src/question_2/main.cpp
+++ src/question_2/main.cpp
@@ -3,76 +3,11 @@
 
 using std::cin; using std::cout;
 
-bool test_config()
+int main()
 {
-    return true;
-}
-
-
- 
-
-int get_fib_number(int num)
-{
-   int num1 = 0;
-   int num2 = 1 ;
-   int next_term = 0;
     
-         if(num < 2) 
-            return num;
-
-        for(int i = 1; i < num; i++)
-        {
-            next_term = num1 + num2;
-            num1 = num2;
-            num2 = next_term;
-        }
-        return next_term;
-}
+display_get_fib_number();
 
 
-void menu()
-{
-    cout << "1- Fib sequence \n";
-    cout << "2- Exit program  \n";
-}
-
-void display_get_fib_number()
-{
-    auto option = 0;
-do
-    {
-        menu();
-       
-        cout << "Enter number: ";
-        cin >> option;
-        handle_menu_option(option);
-       
-
-    } while ( option >= 1 || option <=15 || option == 0);
-}
-
-
-void handle_menu_option(int option)
-{
-
-    auto number = 0;
-
-   switch (option)
-    {
-        case 1:
-            cout << "Enter a number: ";
-            cin >> number;
-            cout << get_fib_number(number) << "\n";
-            break;
-
-        case 2:
-            cout << "Exiting Program... ";
-            abort();
-            break;
-
-        default:
-        cout << "Invalid number!\n";
-
-}
-
+    return 0;
 }
